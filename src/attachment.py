31bf1be8204390a2742f7b5c847a7289e53442cb
jeremy <poff23180@yahoo.com>
2016-10-27 14:55:41 -0400

    fixed errors in attachment reading where it needed to expect files and mentions, since they're both attachments

--- src/attachment.py
+++ src/attachment.py
@@ -5,6 +5,7 @@ class Attachment:
         self.user_ids = list()
         self.loci = list()
         self.type = None
+        self.file_id = None
     
     def __str__(self):
         return json.dumps(vars(self))
@@ -17,16 +18,31 @@ def map_raw_to_attachment(raw):
     try:      
         m = raw["m"]
         # loci = raw["loci"]
-        i_loci = m["loci"]["l"]
-        i_ids = m["user_ids"]["l"]
-        ret.type = m["type"]["s"]
 
-        for tmp in i_ids:
-            ret.user_ids.append(tmp["s"])
-        for tmp in i_loci:
-            tmp = tmp["l"]
-            for inner_tmp in tmp:
-                ret.loci.append(inner_tmp["n"])
+        print "m:  ", m.keys()
+
+        if ( "loci" in m ):
+            i_loci = m["loci"]["l"]
+            for tmp in i_loci:
+                print "key: ", tmp
+                print "value: ", tmp["l"]
+                for item in tmp["l"]:
+                    print "item to append: ", item["n"]
+                    ret.loci.append(item["n"])
+            print "loci:  ", m["loci"]["l"]
+
+        if "user_ids" in m:
+            i_ids = m["user_ids"]["l"]
+            for tmp in i_ids:
+                ret.user_ids.append(tmp["s"])
+           
+        if "type" in m:
+            ret.type = m["type"]["s"]
+
+        if "file_id" in m:
+            print "file_id:  ", m["file_id"]
+            ret.file_id = m["file_id"]["s"]
+
     except Exception as ex:
         print "ERROR mapping raw: ", ex
         raise ex
